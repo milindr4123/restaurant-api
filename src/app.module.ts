5ad0a78efdef55dcededb87b85b6673cd5529246
milindr4123 <e.foreros@uniandes.edu.co>
2025-05-16 10:13:42 -0500

    creation of modules (#1)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RestaurantModule } from './restaurant/restaurant.module';
+import { DishModule } from './dish/dish.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'restaurant.db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    RestaurantModule,
+    DishModule,
+  ],
 })
 export class AppModule {}
---
+++ src/restaurant/restaurant.dto/dish.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany } from 'typeorm';
+import { Restaurant } from '../restaurant.entity/restaurant.entity';
+
+@Entity()
+export class Dish {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column('float')
+  price: number;
+
+  @Column()
+  category: string;
+
+  @ManyToMany(() => Restaurant, restaurant => restaurant.dishes)
+  restaurants: Restaurant[];
+}
